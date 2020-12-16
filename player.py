bb7e20acafe62c66555a3b014ea3fe105fa8b44e
WilliamOtieno <jimmywilliamotieno@gmail.com>
2020-12-16 22:45:33 +0300

    added genius comp

--- player.py
+++ player.py
@@ -50,6 +50,33 @@ class  HumanPlayer(Player):
             
         return val
 
+class GeniusComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+    
+    def get_move(self, game):
+        if len(game.available_moves()) == 9:
+            square = random.choice(game.available_moves())
+        else:
+            square = self.minimax(game, self.letter)
+        return square
+    
+    def minimax(self, state, player):
+        max_player = self.letter
+        other_player = 'O' if player == 'X' else 'X'
+
+        if state.current_winner == other_player:
+            return {'position': None, 
+                    'score': 1  * (state.num_empty_squares() + 1) if other_player == max_player else -1 * (state.num_empty_squares() + 1)
+                    }
+        elif not state.empty_squares():
+            return {'position:' None, 'score': 0}
 
 
+        if player == max_player:
+            best = {'position': None, 'score': -math.inf}
+        else:
+            best = {'position': None, 'score': math.inf}
 
+        for possible_move in state.available_moves():
+            
